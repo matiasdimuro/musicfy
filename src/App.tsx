24266bbeb2be501679787f5c8c80b4ce90904f54
Matias Di muro <66931777+matiasdimuro@users.noreply.github.com>
2023-09-26 09:35:37 -0300

    Upload source files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Master from './layout/Master'
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <div className='min-w-full h-screen bg-gradientPrimary'>
+      <Master/>
+    </div>
+  )
+}
+
+export default App
